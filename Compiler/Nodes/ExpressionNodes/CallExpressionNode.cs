f49f322751100d2c9dfaf4080f06ffd649f1feb1
europaeden <s.stratton@rgu.ac.uk>
2025-06-22 17:18:08 +0100

    Add project files.

---
+++ Compiler/Nodes/ExpressionNodes/CallExpressionNode.cs
@@ -0,0 +1,27 @@
+﻿using Compiler.Tokenization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.Nodes.ExpressionNodes
+{
+  public class CallExpressionNode : IExpressionNode
+    {
+       public IdentifierNode Identifier { get; }
+
+       public IParameterNode Parameter { get; }
+
+        public SimpleTypeDeclarationNode Type { get; set; }
+
+        public Position Position { get { return Identifier.Position; } }
+
+        public CallExpressionNode(IdentifierNode identifier, IParameterNode parameter)
+        {
+            Identifier = identifier;
+            Parameter = parameter;
+            
+        }
+    }
+}
---
+++ Compiler/Nodes/ParameterNodes/ValueParameterNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.Nodes.ParameterNodes
+{
+    public class ValueParameterNode : IParameterNode
+    {
+        public Position Position { get; }
+
+        public SimpleTypeDeclarationNode TypeDeclaration { get; set; }
+
+        public ValueParameterNode(Position position)
+        {
+            Position = position;
+        }
+
+       
+    }
+}
